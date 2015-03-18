46fcf7bcfcd0f5aef67c54ceaf3a40d301df7ce7
unknown <bxh03030@CLK310-07.vtc.vsc.edu>
2015-03-17 20:40:31 -0400

    lab 4 finally done

--- Assignments/Lab_4/lab_4.php
+++ Assignments/Lab_4/lab_4.php
@@ -23,6 +23,8 @@ define('GRAVITY', 9.8);
 
 function truncateFloat($float_value)
 {
+	$float_value = ($float_value * 100) / (100);
+echo $float_value;
 
 }
 
@@ -31,7 +33,8 @@ function truncateFloat($float_value)
  */
 function farenheit2Kelvin($degrees_f)
 {
-
+$degrees_k = ($degrees_f - 32) * (5/9) + 273.15;
+echo $degrees_k;
 }
 
 /**
@@ -39,7 +42,8 @@ function farenheit2Kelvin($degrees_f)
  */
 function dodecahedronVolume($area)
 {
-
+$volume = (((15 + 7) * sqrt(5)) / 4 ) * pow($area,3);
+	echo $volume;
 }
 
 /**
@@ -47,9 +51,32 @@ function dodecahedronVolume($area)
  */
 function impactVelocity($height)
 {
-
+$velocity = sqrt((2)*(GRAVITY)*($height)); 
+	echo $velocity;
+}
+if (isset($_POST['submit'])) {
+    $truncateFloatResult = $_POST["truncateFloat"];
+    $farenheit2KelvinResult = $_POST["farenheit2Kelvin"];
+    $dodecahedronVolumeResult = $_POST["dodecahedronVolume"];
+    $impactVelocityResult = $_POST["impactVelocity"];
+} else {
+    $truncateFloatResults = "";
+    $farenheit2KelvinResults = "";
+    $dodecahedronVolumeResults = "";
+    $impactVelocityResults = "";
 }
 
+$form_layout = <<< EOD
+<p>
+<form method="post" action="">
+<label name="truncateFloat">Floating Point Value</label><input type="text" name="truncateFloat"><br>
+<label name="farenheit2Kelvin">Farenheit Value</label><input type="text" name="farenheit2Kelvin"><br>
+<label name="dodecahedronVolume">Dodecahedron Side Value</label><input type="text" name="dodecahedronVolume"><br>
+<label name="impactVelocity">Height of Fall Value</label><input type="text" name="impactVelocity"><br>
+<input type="submit" value="submit" name="submit">
+</form>
+</p>
+EOD;
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +85,22 @@ function impactVelocity($height)
     <title>Class 4 Lab</title>
 </head>
 <body>
-
+<?php
+if (!isset($_POST['submit'])) {
+    // display the form
+    echo $form_layout;
+} else {
+		echo "<br> \n the truncated floating point value is:";
+		truncateFloat($truncateFloatResult);
+		echo "<br> \n the Kelvin value is:";
+		farenheit2Kelvin($farenheit2KelvinResult);
+		echo "<br> \n the dodecahedron volume value is:";
+		dodecahedronVolume($dodecahedronVolumeResult);
+		echo "<br> \n the impact velocity is:";
+		impactVelocity($impactVelocityResult);
+}
+	
+	
+?>
 </body>
 </html>
